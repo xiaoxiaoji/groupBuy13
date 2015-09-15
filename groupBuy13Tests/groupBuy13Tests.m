80bc78ed761eb5ff04a0d35d087b0872d2956d80
耿米民管会 <gengmiminguanhui@xiaojijideMacBook-Pro.local>
2015-09-15 15:10:59 +0800

    Initial Commit

---
+++ groupBuy13Tests/groupBuy13Tests.m
@@ -0,0 +1,40 @@
+//
+//  groupBuy13Tests.m
+//  groupBuy13Tests
+//
+//  Created by 耿米民管会 on 15/9/15.
+//  Copyright (c) 2015年 gmh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface groupBuy13Tests : XCTestCase
+
+@end
+
+@implementation groupBuy13Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
